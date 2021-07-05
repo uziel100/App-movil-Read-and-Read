cb64a5075873e8f1d872f60634c702957c51c0bb
uziel100 <uziemelitonh@gmail.com>
2021-07-04 19:45:46 -0500

    Screeen stack for view book

---
+++ src/screens/Book/ViewBook.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { WebView } from "react-native-webview";
+import { URL_FILES } from "../../utils/constants";
+
+export default function ViewBook({ route: { params } }) {
+    const navigation =  useNavigation();
+
+    const { fileName, title } = params;
+
+    useEffect(()=> {
+        navigation.setOptions({ title });
+    }, [])
+
+    
+    return (
+        <View style={styles.container}>
+            <WebView
+                source={{
+                    uri: `https://docs.google.com/viewer?url=${ URL_FILES }/${ fileName }`,
+                }}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1        
+    },
+});
